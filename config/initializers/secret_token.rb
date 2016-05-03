ba35001176a2c3ab1e9eb8f22fb56348edd34956
desvia <desvia@hotmail.it>
2016-05-03 17:41:49 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Desvia::Application.config.secret_token = 'b4620d72564de2b68536a1e60266bf924216e9512c0e957c6606940c985458b925004991c816801aaf6bc110d44934258f69308ab3e19aaa413fcdc970b7a19a'
